0e7311e88617a25c49a944d573bd198bd3e05342
SonDN <sondoannam202@gmail.com>
2024-09-18 18:04:19 +0700

    feat: learn simple skeleton of controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /**
+   * GET /users
+   * GET /users/:id
+   * POST /users
+   * PATCH /users/:id
+   * DELETE /users/:id
+   */
+
+  @Get() // GET /users
+  findAll(@Query('role') role?: 'admin' | 'user') {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return {
+      id,
+      name: 'John Doe',
+      email: 'example@email.com',
+    };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: { name: string; email: string }) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(
+    @Param('id') id: string,
+    @Body() user: { name: string; email: string },
+  ) {
+    return {
+      id,
+      ...user,
+    };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
